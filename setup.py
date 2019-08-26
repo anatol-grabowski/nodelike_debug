8c177812f5388a95348bac7d4f3117c177774b13
tot <grabantot@gmail.com>
2019-08-26 22:33:34 +0300

    pypi uploadable

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'nodelike_debug',         # How you named your package folder (MyLib)
-  packages = ['nodelike_debug'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='nodelike_debug',
+    packages=['.nodelike_debug'],
+    version='0.1',
+    description='nodelike_debug: a python version of debug package for node',
+    long_description='descr',
+    long_description_content_type='text/plain',
+    author='Anatol Grabowski',
+    url='https://github.com/grabantot/nodelike_debug',
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ]
 )
